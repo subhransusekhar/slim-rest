6f4bd15db895e41f6d5526377219993ec5fe73c3
Subhransu Sekhar <subhransu.wsd@nic.in>
2016-09-30 12:47:43 +0530

    Middle ware validate from MyGov auth

--- src/middleware.php
+++ src/middleware.php
@@ -2,15 +2,15 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
 
 class AuthenticationMiddleware
 {
     public function __invoke($request, $response,  $next)
     {
       if ($request->hasHeader('Authorization')) {
-          if($request->getHeaderLine('Authorization') == 'Get me in') {
+          $request_token = str_replace("OAuth2 ", "", $request->getHeaderLine('Authorization'));
+          $code = $this->_verify_auth_token($request_token);
+          if($code == 200) {
               return $next($request, $response);
           }
           return $this->error(403);
@@ -27,7 +27,7 @@ class AuthenticationMiddleware
           $result = array("status" => "403", "message" => "Authorization Error!");
           header("Content-Type: application/json");
           echo json_encode($result);
-        exit;
+          exit;
           break;
         case '500':
         default:
@@ -39,5 +39,19 @@ class AuthenticationMiddleware
           break;
       }
 
+    }
+    private function _verify_auth_token($token) {
+      $url = 'https://auth.mygov.in/oauth2/tokens/' . $token;
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+      curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,false);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
+      curl_setopt($ch, CURLOPT_TIMEOUT,10);
+      $output = curl_exec($ch);
+      $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      curl_close($ch);
+      return $httpcode;
     }
 }
